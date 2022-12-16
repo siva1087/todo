9f2b4c057d6b8c464f0dd672617370ea625f0928
objmedsyssiva <siva@objectivemedicalsystems.com>
2022-12-16 12:59:08 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,116 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      :mobile-breakpoint="768"
+      app
+    >
+      <v-img
+        class="pa-4 pt-7"
+        src="rs.jpg"
+        height="170"
+        
+      >
+        <v-avatar size="70" class="mb-2">
+          <img
+            src="sivakumar.jpg"
+            alt="Sivakumar"
+          >
+        </v-avatar>
+        <div class="white--text text-subtitle-1 font-weight-bold">
+          Sivakumar
+        </div>
+        <div class="white--text text-subtitle-2">
+          Baskar
+        </div>
+      </v-img>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          :to="item.to"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      
+      dark
+      src="rs.jpg"
+      prominent
+    height="170"
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+  
+        ></v-img>
+      </template>
+
+      <v-container class="header-container pa-0">
+        <v-row>
+          <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+          <v-spacer></v-spacer>
+          <search />
+        </v-row>
+        <v-row>
+          <v-toolbar-title class="text-h4 ml-4">
+            {{ $store.state.appTitle }}
+          </v-toolbar-title>
+        </v-row>
+        <v-row>
+          <live-date-time />
+        </v-row>
+        <v-row v-if="$route.path === '/'">
+          <field-add-task />
+        </v-row>
+      </v-container>
+
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+      <snackbar />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: null,
+      items: [
+        { title: 'Todo', icon: 'mdi-format-list-checks', to: '/' },
+        { title: 'About', icon: 'mdi-help-box', to: '/about' },
+      ],
+    }),
+    mounted() {
+      this.$store.dispatch('getTasks')
+    },
+    components: {
+      'search': require('@/components/Tools/Search.vue').default,
+      'live-date-time': require('@/components/Tools/LiveDateTime.vue').default,
+      'field-add-task': require('@/components/Todo/FieldAddTask.vue').default,
+      'snackbar': require('@/components/Shared/Snackbar.vue').default
+    }
+  }
+</script>
+
+<style lang="sass">
+  .header-container
+    max-width: none
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about pa-6">
+    <h1>{{$store.state.appTitle }}</h1>
+    <p >Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut fugit, incidunt dolorem neque eaque nobis perferendis reiciendis pariatur dolore, voluptate earum ab doloribus eum alias autem laborum repellendus eius dolores.</p>
+ <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit neque mollitia incidunt dolores quidem et iste nisi deserunt eligendi excepturi voluptate quod, in nobis eius officia illo rem totam quaerat!</p>
+ <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore exercitationem distinctio laborum veniam quaerat dolore minus cumque praesentium saepe modi consequatur fuga quas ex natus corrupti, magni maiores non qui?</p>
+ 
+  </div>
+</template>
